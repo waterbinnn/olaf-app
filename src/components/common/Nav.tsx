948bb35271dd2f19129d79e0badd3d6d424ec731
waterbinnn <ewaterbin@naver.com>
2022-12-08 16:01:56 +0900

    route 에 따라 nav active 색상 고정되게 구현

--- src/components/common/Nav.tsx
+++ src/components/common/Nav.tsx
@@ -1,19 +1,33 @@
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 
 import './Nav.scss';
 
 const Nav = () => {
+  const location = useLocation();
+  const [path, setPath] = useState('');
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setPath('/');
+    } else if (location.pathname === '/addTodo') {
+      setPath('/addTodo');
+    } else {
+      setPath('/news');
+    }
+  }, [location]);
+
   return (
     <footer className="footer">
       <nav className="nav-container">
         <ul className="nav-wrapper">
-          <li className="nav-item">
+          <li className={path === '/addTodo' ? 'nav-item active' : 'nav-item'}>
             <Link to={'addTodo'}>TODO</Link>
           </li>
-          <li className="nav-item">
+          <li className={path === '/' ? 'nav-item active' : 'nav-item'}>
             <Link to={'/'}>HOME</Link>
           </li>
-          <li className="nav-item">
+          <li className={path === '/news' ? 'nav-item active' : 'nav-item'}>
             <Link to={'news'}>NEWS</Link>
           </li>
         </ul>
