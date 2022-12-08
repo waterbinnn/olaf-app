f78e10b1a6783df3d678f96ad76b76c10d71265d
waterbinnn <ewaterbin@naver.com>
2022-12-08 11:08:02 +0900

    Home component wish 마크업 및 스타일링

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Olaf from '../components/olaf/Olaf';
 import Header from '../components/Header';
 
-import { dateString } from '../common/date';
+import { dateString, weekday } from '../common/date';
 
 import './Home.scss';
 
@@ -12,7 +12,18 @@ const Home = () => {
       <Header type="home">OLAF!</Header>
       <main className="main">
         <p className="date">{dateString}</p>
+        <p className="date-week">오늘은 {weekday}요일 이에요.</p>
         <Olaf />
+        <form className="wish-form">
+          <label htmlFor="text">오늘의 소원을 적어보세요.</label>
+          <input type="text" id="text" />
+          <button type="submit">🥕</button>
+        </form>
+        <div className="user-wish">
+          <span>Your Wish</span>
+          {/* 작성한 당일에만 나올 수 있게 구현 */}
+          <p>회사에서 연락이 좀 왔으면 좋겠다 .. </p>
+        </div>
       </main>
     </>
   );
