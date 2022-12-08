e1627e6a98ff94dab2742abfd775d14a1ce19b61
waterbinnn <ewaterbin@naver.com>
2022-12-08 11:06:07 +0900

    weekday 추가 작성

--- src/common/date.ts
+++ src/common/date.ts
@@ -3,4 +3,7 @@ let year = today.getFullYear();
 let month = ('0' + (today.getMonth() + 1)).slice(-2);
 let day = ('0' + today.getDate()).slice(-2);
 
+const week = ['일', '월', '화', '수', '목', '금', '토'];
+
+export let weekday = week[today.getDay()];
 export let dateString = year + '년 ' + month + '월 ' + day + '일';
