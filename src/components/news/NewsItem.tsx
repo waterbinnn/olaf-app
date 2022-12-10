70af393aa3ea6bca21b8ca771ae1a132c03f85c0
waterbinnn <ewaterbin@naver.com>
2022-12-10 17:29:58 +0900

    Fixed image bug

--- src/components/news/NewsItem.tsx
+++ src/components/news/NewsItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './NewsItem.scss';
 
 type NewsItemProps = {
@@ -6,19 +7,24 @@ type NewsItemProps = {
 
 const NewsItem = (props: NewsItemProps) => {
   const { data } = props;
+  const [image, setImage] = useState('');
 
   const handleClick = () => {
     window.open(data.url, '_blank');
   };
 
+  useEffect(() => {
+    if (data.multimedia) {
+      setImage(data.multimedia[2].url);
+    } else {
+      setImage(data.thumbnail_standard);
+    }
+  }, [data.multimedia, data.thumbnail_standard]);
+
   return (
     <>
       <li className="news-item" key={data.id} onClick={handleClick}>
-        <img
-          className="news-image"
-          src={data.multimedia[2].url}
-          alt={data.multimedia[2].caption}
-        />
+        <img className="news-image" src={image} />
         <div className="news-text-wrapper">
           <strong className="news-title">{data.title}</strong>
           <p className="news-desc">{data.abstract}</p>
