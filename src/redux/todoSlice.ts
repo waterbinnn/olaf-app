0d3512603d25297155b8a6cf0ed76ecb5219201a
waterbinnn <ewaterbin@naver.com>
2022-12-11 18:35:46 +0900

    checkbox reducer 구현

--- src/redux/todoSlice.ts
+++ src/redux/todoSlice.ts
@@ -32,8 +32,24 @@ export const todoSlice = createSlice({
         );
       }
     },
+    checkBox: (state, action) => {
+      state.todoList.push(action.payload);
+      const todoList = window.localStorage.getItem('todoList');
+
+      if (todoList) {
+        const todoListArr = JSON.parse(todoList);
+        todoListArr.forEach((todo: any) => {
+          if (todo.id === action.payload.id) {
+            todo.completed = action.payload.completed;
+          }
+        });
+        window.localStorage.setItem('todoList', JSON.stringify(todoListArr));
+        state.todoList = [...todoListArr];
+      }
+    },
+    //
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, checkBox } = todoSlice.actions;
 export default todoSlice.reducer;
