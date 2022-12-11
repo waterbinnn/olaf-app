8da01798974626c1a0c41de73b37991303701dcb
waterbinnn <ewaterbin@naver.com>
2022-12-11 18:03:39 +0900

    Add todoSlice

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import todoSlice from './todoSlice';
 import wishSlice from './wishSlice';
 
 export const store = configureStore({
   reducer: {
     wish: wishSlice,
+    todo: todoSlice,
   },
 });
 
