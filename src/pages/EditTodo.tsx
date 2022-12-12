58a74f94044b8010a75cbf1520f1a10875e19a28
waterbinnn <ewaterbin@naver.com>
2022-12-12 14:06:52 +0900

    작성했던 Todo 수정 기능 구현

--- src/pages/EditTodo.tsx
+++ src/pages/EditTodo.tsx
@@ -1,17 +1,53 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { editTodo } from '../redux/todoSlice';
 import Header from '../components/common/Header';
+import { TodoStateType } from '../common/types';
 
 const EditTodo = () => {
   const [title, setTitle] = useState('');
   const [desc, setDesc] = useState('');
+  const [id, setId] = useState('');
 
-  const handleSubmit = () => {};
-  const handleChange = () => {};
+  const todoList = useSelector((state: TodoStateType) => state.todo.todoList);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const queryId = location.search.slice(4);
+
+  useEffect(() => {
+    setId(queryId);
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].id.toString() === queryId) {
+        setTitle(todoList[i].title);
+        setDesc(todoList[i].desc);
+      }
+    }
+  }, [queryId, todoList]);
+
+  const handleSubmit = () => {
+    let todoItem = {
+      id: id,
+      title: title,
+      desc: desc,
+    };
+    dispatch(editTodo(todoItem));
+    navigate('/todolist');
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | any): void => {
+    if (e.target.id === 'todoTitle') {
+      setTitle(e.target.value);
+    } else {
+      setDesc(e.target.value);
+    }
+  };
 
   return (
     <>
-      <Header type="todo" link="/eidtTodo">
+      <Header type="todo" link="/editTodo">
         EDIT TODO
       </Header>
       <main className="main">
@@ -34,6 +70,9 @@ const EditTodo = () => {
               rows={10}
             />
           </div>
+          <button type="submit" className="btn-todo">
+            ADD TODO
+          </button>
         </form>
       </main>
     </>
